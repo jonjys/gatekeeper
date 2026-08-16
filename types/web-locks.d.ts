b7bb14e2133c7ce7a20e1b86e01d3c6ce6ef3ef4
FRED <fkornelind@hotmail.com>
2026-08-16 12:53:39 +0200

    feat: WebUSB + showSaveFilePicker types

--- types/web-locks.d.ts
+++ types/web-locks.d.ts
@@ -12,14 +12,67 @@ interface Lock {
 }
 interface Navigator {
   locks: LockManager;
+  usb: USB;
 }
+
+interface USB {
+  requestDevice(options: USBDeviceRequestOptions): Promise<USBDevice>;
+  getDevices(): Promise<USBDevice[]>;
+}
+
+interface USBDeviceRequestOptions {
+  filters: USBDeviceFilter[];
+}
+
+interface USBDeviceFilter {
+  vendorId?: number;
+  productId?: number;
+  classCode?: number;
+  subclassCode?: number;
+  protocolCode?: number;
+  serialNumber?: string;
+}
+
+interface USBDevice {
+  productName?: string;
+  manufacturerName?: string;
+  vendorId: number;
+  productId: number;
+  configuration: USBConfiguration | null;
+  open(): Promise<void>;
+  close(): Promise<void>;
+  selectConfiguration(configurationValue: number): Promise<void>;
+  claimInterface(interfaceNumber: number): Promise<void>;
+}
+
+interface USBConfiguration {
+  configurationValue: number;
+  interfaces: USBInterface[];
+}
+
+interface USBInterface {
+  interfaceNumber: number;
+}
+
 interface Window {
   showOpenFilePicker(options?: {
     multiple?: boolean;
     excludeAcceptAllOption?: boolean;
     types?: Array<{ description?: string; accept: Record<string, string[]> }>;
   }): Promise<FileSystemFileHandle[]>;
+  showSaveFilePicker(options?: {
+    suggestedName?: string;
+    excludeAcceptAllOption?: boolean;
+    types?: Array<{ description?: string; accept: Record<string, string[]> }>;
+  }): Promise<FileSystemFileHandle>;
 }
+
 interface FileSystemFileHandle {
   getFile(): Promise<File>;
+  createWritable(): Promise<FileSystemWritableFileStream>;
+}
+
+interface FileSystemWritableFileStream extends WritableStream {
+  write(data: BufferSource | Blob | string): Promise<void>;
+  close(): Promise<void>;
 }
